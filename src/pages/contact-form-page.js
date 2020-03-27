3d0185d68645f54ca421bc3e98c281a558b015c7
jamesobrien86 <jamesobrien.web@gmail.com>
2020-03-27 17:32:37 +0300

    impl - update contact redux

--- src/pages/contact-form-page.js
+++ src/pages/contact-form-page.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Redirect} from 'react-router';
 import {SubmissionError} from 'redux-form';
 import {connect} from 'react-redux';
-import { newContact, saveContact } from '../actions/contact-actions';
+import { newContact, saveContact, fetchContact, updateContact } from '../actions/contact-actions';
 import ContactForm from '../components/contact-form';
 
 class ContactFormPage extends Component{
@@ -12,15 +12,28 @@ class ContactFormPage extends Component{
     }
 
     componentDidMount(){
-        this.props.newContact();
+        const {_id} = this.props.match.params;
+        if(_id){
+            this.props.fetchContact(_id);
+        }else{
+            this.props.newContact();
+        }
     }
 
     submit = (contact) => {
+       if(!contact._id){
         return this.props.saveContact(contact)
         .then(response => this.setState({redirect:true}))
         .catch(err => {
             throw new SubmissionError(this.props.errors)
         })
+       }else{
+           return this.props.updateContact(contact)
+           .then(response => this.setState({redirect:true}))
+           .catch(err => {
+               throw new SubmissionError(this.props.errros)
+           })
+       }
     }
 
     render(){
@@ -44,4 +57,4 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps,{newContact,saveContact})(ContactFormPage);
+export default connect(mapStateToProps,{newContact,saveContact, fetchContact, updateContact})(ContactFormPage);
--- src/reducers/contact-reducer.js
+++ src/reducers/contact-reducer.js
@@ -50,6 +50,47 @@ const defaultState = {
          }
 
       }
+      case 'FETCH_CONTACT_PENDING':{
+        return {
+            ...state,
+            loading:true,
+            contact:{name:{}}
+        }
+      }
+      case 'FETCH_CONTACT_FULFILLED':{
+        return {
+            ...state,
+            contact:action.payload.data,
+            errors:{},
+            loading:false
+        }
+      }
+      case 'UPDATE_CONTACT_PENDING':{
+        return{
+            ...state,
+            loading:true
+        }
+      }
+      case 'UPDATE_CONTACT_FULFILLED':{
+          const contact = action.payload.data
+          return {
+              ...state,
+              contacts:state.contacts.map(item => item._id === contact._id ? contact:item),
+              errors:{},
+              loading:false
+          }
+      }
+      case 'UPDATE_CONTACT_REJECTED': {
+        const data = action.payload.response.data;
+        //convet feathers formatting to match client side error formatting
+        const { "name.first":first, "name.last":last,phone,email} = data.errors;
+        const errors = {global:data.message, name:{first,last}, phone,email};
+        return {
+            ...state,
+            errors:errors,
+            loading:false
+        }
+      }
       default:
         return state;
     }
